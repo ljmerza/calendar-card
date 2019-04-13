3f16454d75e7f788c5d24c5956feed869e09c80c
Leonardo Merza <ljmerza@gmail.com>
2019-04-12 21:20:44 -0400

    version bump

--- babel.config.js
+++ babel.config.js
@@ -5,17 +5,7 @@ module.exports = {
             {
                 "useBuiltIns": "usage",
                 "debug": true,
-                "targets": {
-                    "android": "4.4.3",
-                    "chrome": "50",
-                    "edge": "17",
-                    "firefox": "64",
-                    "ie": "10",
-                    "ios": "8",
-                    "opera": "57",
-                    "safari": "11.1",
-                    "samsung": "4"
-                },
+                "targets": "> 0.25%, not dead",
                 "shippedProposals": true
             }
         ]
