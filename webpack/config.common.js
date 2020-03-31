5fe2db6d09aeb7a96461bc0527abc5a2f4962809
Leonardo Merza <ljmerza@gmail.com>
2020-03-31 15:14:45 -0400

    remove js mapfor prod build

--- webpack/config.common.js
+++ webpack/config.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    devtool: 'source-map',
     output: {
         filename: 'calendar-card.js',
         path: path.resolve(__dirname, '../dist')
